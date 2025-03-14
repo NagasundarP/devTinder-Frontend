e21ac3236fa9de51f547cdc746b50faea4e6ab69
Naga P <nagap@Nagas-MacBook-Air.local>
2025-03-14 12:18:03 +0530

    add login auth to access pages

--- src/components/Body.jsx
+++ src/components/Body.jsx
@@ -2,13 +2,17 @@ import { Outlet } from "react-router-dom";
 import NavBar from "./NavBar";
 import { BASE_URL } from "../utils/constants";
 import axios from "axios";
-import { useDispatch } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import { addUser } from "../utils/userSlice";
 import { useEffect } from "react";
+import { useNavigate } from "react-router-dom";
 
 const Body = () => {
   const dispatch = useDispatch();
+  const navigate = useNavigate();
+  const userData = useSelector((state) => state.user);
   const fetchUser = async () => {
+    if(userData) return;
     try {
       const res = await axios.get(BASE_URL + "profile", {
         withCredentials: true,
@@ -16,12 +20,15 @@ const Body = () => {
       console.log(res);
       dispatch(addUser(res.data));
     } catch (error) {
+      if (error.status === 401) {
+        return navigate("/login");
+      }
       console.log(error);
     }
   };
 
   useEffect(() => {
-    fetchUser();
+      fetchUser();
   }, []);
 
   return (
