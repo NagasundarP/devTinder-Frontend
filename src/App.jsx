6138d6bf01ac8c7ba38d1567f5e9d5dc5a83ffe2
Naga P <nagap@Nagas-MacBook-Air.local>
2025-03-24 12:09:33 +0530

    made conections page

--- src/App.jsx
+++ src/App.jsx
@@ -5,20 +5,23 @@ import { Provider } from "react-redux";
 import appStore from "./utils/appStore";
 import Feed from "./components/UserFeed.jsx";
 import Profile from "./components/Profile.jsx";
+import Connections from "./components/Connections.jsx";
 
 function App() {
   return (
     <>
-    <Provider store={appStore}>
-      <BrowserRouter basename="/">
-        <Routes>
-          <Route path="/" element={<Body />}>
-            <Route path="/login" element={<Login />} />
-            <Route path="/feed" element={<Feed />} />
-            <Route path="/profile" element={<Profile />} />
-          </Route>
-        </Routes>
-      </BrowserRouter>
+      <Provider store={appStore}>
+        <BrowserRouter basename="/">
+          <Routes>
+            <Route path="/" element={<Body />}>
+              <Route path="/login" element={<Login />} />
+              <Route path="/feed" element={<Feed />} />
+              <Route path="/profile" element={<Profile />} />
+              <Route path="/requests" element={<Connections />} />
+              <Route path="/connections" element={<Connections />} />
+            </Route>
+          </Routes>
+        </BrowserRouter>
       </Provider>
     </>
   );
---
+++ src/components/Connections.jsx
@@ -0,0 +1,54 @@
+import axios from "axios";
+import { BASE_URL } from "../utils/constants";
+import { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { addConnections } from "../utils/connectionsSlice";
+
+const Connections = () => {
+  const dispatch = useDispatch();
+  const connectionsReq = useSelector((store) => store.connection);
+
+  const fetchConnections = async () => {
+    try {
+      const res = await axios.get(BASE_URL + "user/connections", {
+        withCredentials: true,
+      });
+      dispatch(addConnections(res?.data));
+    } catch (err) {
+      console.log(err);
+    }
+  };
+
+  useEffect(() => {
+    fetchConnections();
+  }, []);
+
+  if (!connectionsReq) return;
+  if (connectionsReq.length === 0) return <h1>No Connections Found</h1>;
+  return (
+    <div>
+      <div className="text-center my-10">
+        <h1 className="text-bold text-2xl">Connections</h1>
+        {connectionsReq &&
+          connectionsReq.map((connection) => {
+            return (
+              <div
+                className="flex m-4 p-4 rounded-lg bg-base-200"
+                key={connection._id}
+              >
+                <div>
+                  <img src={connection.photoUrl} className="w-50 h-50 mr-4 rounded-xl" />
+                </div>
+                <div>
+                  <div>{connection.firstName}</div>
+                  <div>{connection.about}</div>
+                </div>
+              </div>
+            );
+          })}
+      </div>
+    </div>
+  );
+};
+
+export default Connections;
