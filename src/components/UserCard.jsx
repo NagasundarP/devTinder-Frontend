0d0636e79a790c93d3e6778917fae64b65ceaab4
Naga P <nagap@Nagas-MacBook-Air.local>
2025-04-01 11:35:57 +0530

    build the feed connections flow

--- src/components/UserCard.jsx
+++ src/components/UserCard.jsx
@@ -1,26 +1,55 @@
+import axios from "axios";
+import { BASE_URL } from "../utils/constants";
+import { useDispatch } from "react-redux";
+import { removeFeed } from "../utils/feedSlice";
+
 const UserCard = ({ user }) => {
-  console.log(user);
+  const dispatch = useDispatch();
+  const handleRequest = async (status, _id) => {
+    try {
+      const res = await axios.post(
+        BASE_URL + "request/send/" + status + "/" + _id,
+        {},
+        {
+          withCredentials: true,
+        }
+      );
+      dispatch(removeFeed(_id));
+    } catch (err) {
+      console.log(err);
+    }
+  };
+
   return (
     <div className="flex flex-wrap justify-center gap-4">
-      {user &&
-        user.map((item) => (
-          <div className="card bg-base-300 w-96 shadow-sm" key={item.firstName}>
-            <figure>
-              <img src={item.photoUrl} alt="Shoes" />
-            </figure>
-            <div className="card-body">
-              <h2 className="card-title">
-                {item.firstName} {item.lastName}
-              </h2>
-              <p>{item.age}</p>
-              <p>{item.about}</p>
-              <div className="card-actions justify-center">
-                <button className="btn btn-primary">Interested</button>
-                <button className="btn btn-primary">Ignore</button>
-              </div>
+      {user && (
+        <div className="card bg-base-300 w-96 shadow-sm" key={user.firstName}>
+          <figure>
+            <img src={user.photoUrl} alt="Shoes" />
+          </figure>
+          <div className="card-body">
+            <h2 className="card-title">
+              {user.firstName} {user.lastName}
+            </h2>
+            <p>{user.age}</p>
+            <p>{user.about}</p>
+            <div className="card-actions justify-center">
+              <button
+                className="btn btn-primary"
+                onClick={() => handleRequest("interested", user._id)}
+              >
+                Interested
+              </button>
+              <button
+                className="btn btn-primary"
+                onClick={() => handleRequest("ignored", user._id)}
+              >
+                Ignore
+              </button>
             </div>
           </div>
-        ))}
+        </div>
+      )}
     </div>
   );
 };
